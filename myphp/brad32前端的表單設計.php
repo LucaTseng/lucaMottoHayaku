0a76d0438d874120a04c13b5533a05755b640e67
Luca <86730662+lucaMottoHayaku@users.noreply.github.com>
2021-09-03 20:31:57 +0800

    上課筆記

--- myphp/brad32前端的表單設計.php
+++ myphp/brad32前端的表單設計.php
@@ -1,4 +1,4 @@
-<form action='brad33.php' method="get">
+<form action='brad33.php' method="post">
     Account: <input type="text" name="account" /><br />
     Password: <input type="password" name="passwd" /><br />
     Zipcode: <select name="zipcode">
@@ -7,8 +7,10 @@
         <option value='403'>南屯區</option>
         <option value='404'>東屯區</option>
     </select><br />
-    Gender: <input type="radio" name="gender" value="f"/>Female
-    <input type="radio" name="gender" value="m"/>Male<br />
+    Gender:
+    <input type="radio" name="gender" value="f" />Female
+    <input type="radio" name="gender" value="m" />Male
+    <br />
     Habit: <br />
     <input type="checkbox" name="habit[]" value="1" />打電腦
     <input type="checkbox" name="habit[]" value="2" />打電動
@@ -21,3 +23,9 @@
     <hr />
     <input type="submit" value="Register" />
 </form>
+<!-- // 第一行我寫了傳遞方式get，這樣的傳遞方式是使用url進行傳遞 -->
+<!-- 最好的方式 是使用post進行傳遞 這樣才不會出現在網址上面 -->
+<!-- 第一頁 我們寫了使用 怎麼樣的方式傳遞，後面就應該也要使用怎麼樣的方式進行 -->
+<!-- 第一行 假如我們使用get 第二頁的第二行就要使用get -->
+<!-- 第一行 假如我們使用post 第二頁的第二行就要使用post -->
+<!-- 或者，前端怎麼寫 都沒關西 後面使用$_REQUEST 這樣也都行 -->
